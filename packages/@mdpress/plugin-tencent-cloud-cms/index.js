d1b12358d3d499fd72f1d51156c7f76e26f96b1c
docschina1024@gmail.com <docschina1024@gmail.com>
2021-03-04 17:17:17 +0800

    refactor: tencent cms plugin

--- packages/@mdpress/plugin-tencent-cloud-cms/index.js
+++ packages/@mdpress/plugin-tencent-cloud-cms/index.js
@@ -2,7 +2,6 @@ const { path } = require('@mdpress/shared-utils');
 
 const Plugin = (options) => {
   return {
-    name: 'mdpress-plugin-online',
     enhanceAppFiles: path.resolve(__dirname, 'client/index.js'),
     define: {
       TENCENT_CLOUD_ENV: options.tencentCloudEnv,
