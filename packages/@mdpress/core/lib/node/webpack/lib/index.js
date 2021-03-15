f4ee1cc5830213f9f15a45f9f8d3d6b0dd2d348d
docschina1024@gmail.com <docschina1024@gmail.com>
2021-03-15 16:25:41 +0800

    refactor: @mdpress/core react-server-render

--- packages/@mdpress/core/lib/node/webpack/lib/index.js
+++ packages/@mdpress/core/lib/node/webpack/lib/index.js
@@ -1,8 +1,3 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-var create_bundle_renderer_1 = require('./bundle-renderer/create-bundle-renderer');
-var create_renderer_1 = require('./create-renderer');
-exports.createRenderer = create_renderer_1.createRenderer;
-process.env.REACT_ENV = 'server';
-exports.createBundleRenderer = create_bundle_renderer_1.createBundleRendererCreator(create_renderer_1.createRenderer);
-//# sourceMappingURL=index.js.map
+const { createBundleRenderer } = require('@mdpress/shared-utils');
+
+exports.createBundleRenderer = createBundleRenderer;
