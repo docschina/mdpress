bcae299327495fd2cd62162ef860ec3c92d128fc
docschina1024@gmail.com <docschina1024@gmail.com>
2021-03-16 16:55:19 +0800

    refactor: ssr bundle render unit test

--- packages/@mdpress/shared-utils/__tests__/react-server-render/ssr-bundle-render.spec.ts
+++ packages/@mdpress/shared-utils/__tests__/react-server-render/ssr-bundle-render.spec.ts
@@ -1,35 +1,4 @@
-import { compileWithWebpack } from './compile-with-webpack';
-import { createBundleRenderer } from '../../src/react-server-render';
-import { ReactSSRServerPlugin } from '../../src/react-server-render/webpack-plugin/server';
-
-function createRenderer (file, options, cb) {
-  if (typeof options === 'function') {
-    cb = options;
-    options = undefined;
-  }
-  const asBundle = !!(options && options.asBundle);
-  if (options) delete options.asBundle;
-
-  compileWithWebpack(file, {
-    target: 'node',
-    devtool: asBundle ? '#source-map' : false,
-    output: {
-      path: '/',
-      filename: 'bundle.js',
-      libraryTarget: 'commonjs2'
-    },
-    externals: [/^react|react-router-dom$/],
-    plugins: asBundle
-      ? [new ReactSSRServerPlugin()]
-      : []
-  }, fs => {
-    const bundle = asBundle
-      ? JSON.parse(fs.readFileSync('/react-ssr-server-bundle.json', 'utf-8'))
-      : fs.readFileSync('/bundle.js', 'utf-8');
-    const renderer = createBundleRenderer(bundle, options);
-    cb(renderer);
-  });
-}
+import { createRenderer } from './util';
 
 function createAssertions (runInNewContext) {
   it('renderToString', () => { return new Promise(done => {
